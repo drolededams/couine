a1399e4e285f9d8c44ee8d04ea4486eb487b3c71
Damien GAMEIRO <dgameiro@e1r5p1.42.fr>
2018-09-06 12:57:18 +0200

    fix close file

---
+++ C/Colleen.c
@@ -0,0 +1,2 @@
+#include<stdio.h>
+void print(void){char*s="#include<stdio.h>%cvoid print(void){char*s=%c%s%c;printf(s,10,34,s,34,10);}int main (void){/*commentaire fonction main*/print();return 0;}/*commentaire en dehors des fonctions*/%c";printf(s,10,34,s,34,10);}int main (void){/*commentaire fonction main*/print();return 0;}/*commentaire en dehors des fonctions*/
