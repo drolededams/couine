034c737463d353a57fba4c2298370f9e13edf247
Damien GAMEIRO <dgameiro@e1r5p1.42.fr>
2018-09-06 12:58:26 +0200

    rm *.c

--- Colleen.c
+++
@@ -1,2 +0,0 @@
-#include<stdio.h>
-void print(void){char*s="#include<stdio.h>%cvoid print(void){char*s=%c%s%c;printf(s,10,34,s,34,10);}int main (void){/*commentaire fonction main*/print();return 0;}/*commentaire en dehors des fonctions*/%c";printf(s,10,34,s,34,10);}int main (void){/*commentaire fonction main*/print();return 0;}/*commentaire en dehors des fonctions*/
--- Grace.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-#define FICHIER fich
-#define CHAINE s
-#define GRACE(x) int main (void){FILE* FICHIER = NULL;char*CHAINE="#include<stdio.h>%c#define FICHIER fich%c#define CHAINE s%c#define GRACE(x) int main (void){FILE* FICHIER = NULL;char*CHAINE=%c%s%c;FICHIER = fopen(%cGrace_kid.c%c, %cw+%c);if (FICHIER != NULL){fprintf(FICHIER, CHAINE, 10, 10, 10, 34, CHAINE, 34, 34, 34, 34, 34, 10, 10, 10);fclose(FICHIER);}return 0;}%c/*commentaire*/%cGRACE()%c";FICHIER = fopen("Grace_kid.c", "w+");if (FICHIER != NULL){fprintf(FICHIER, CHAINE, 10, 10, 10, 34, CHAINE, 34, 34, 34, 34, 34, 10, 10, 10);fclose(FICHIER);}return 0;}
-/*commentaire*/
-GRACE()
