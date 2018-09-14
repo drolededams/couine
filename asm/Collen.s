4d1a91f2b7cc079f7f9c98701753583ff30b54c2
Damien GAMEIRO <dgameiro@e1r7p1.42.fr>
2018-09-14 15:33:45 +0200

    Colleen segf fix

--- asm/Collen.s
+++ asm/Collen.s
@@ -1,15 +1,16 @@
 section .data
 txt:
-db "section .data%ctxt:%cdb %c%s%c%cglobal _main%cextern _printf%csection .text%c_main:%c;commentaire fonction main%cmov rcx, 0%cnew_line:%cpush 10%ccmp rcx, 16%cje print%cinc rcx%cjmp new_line%cprint:%clea rdi, [rel txt]%cmov rsi, 10%cmov rdx, 10%cmov rcx, 34%clea r8 [rel txt]%cmov r9, 34%cxor rax, rax%ccall _printf%cret%c;commentaire hors fonction%c"
+db "section .data%ctxt:%cdb %c%s%c%cglobal _main%cextern _printf%csection .text%c_main:%c;commentaire fonction main%cmov rbp, rsp%cmov rcx, 0%cnew_line:%cpush 10%ccmp rcx, 26%cje print%cinc rcx%cjmp new_line%cprint:%clea rdi, [rel txt]%cmov rsi, 10%cmov rdx, 10%cmov rcx, 34%clea r8, [rel txt]%cmov r9, 34%cxor rax, rax%ccall _printf%cmov rsp, rbp%cret%c;commentaire hors fonction%c"
 global _main
 extern _printf
 section .text
 _main:
 ;commentaire fonction main
+mov rbp, rsp
 mov rcx, 0
 new_line:
 push 10
-cmp rcx, 24
+cmp rcx, 26
 je print
 inc rcx
 jmp new_line
@@ -22,5 +23,6 @@ lea r8, [rel txt]
 mov r9, 34
 xor rax, rax
 call _printf
+mov rsp, rbp
 ret
 ;commentaire hors fonction
