464c0cd994d2f2be70fa19b4e4e91dcbbf3e2315
Damien GAMEIRO <dgameiro@e1r12p1.42.fr>
2018-09-19 13:01:45 +0200

    add Sully py

--- python/Grace.py
+++ python/Grace.py
@@ -2,11 +2,11 @@
  commentaire hors fonction
 """
 define_1 = 'Grace_kid.py'
-define_2 = 'w'
+define_2 = 'w+'
 
 def print_it(X):
  f = open(define_1, define_2)
- s = "%c%c%c%c commentaire hors fonction%c%c%c%c%cdefine_1 = 'Grace_kid.py'%cdefine_2 = 'w'%c%cdef print_it(X):%c f = open(define_1, define_2)%c s = %c%s%c%c f.write(s %c (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))%c f.close()%c%cprint_it(1)%c"
+ s = "%c%c%c%c commentaire hors fonction%c%c%c%c%cdefine_1 = 'Grace_kid.py'%cdefine_2 = 'w+'%c%cdef print_it(X):%c f = open(define_1, define_2)%c s = %c%s%c%c f.write(s %c (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))%c f.close()%c%cprint_it(1)%c"
  f.write(s % (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))
  f.close()
 
