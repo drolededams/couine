256a7d81bc53bb73c509cc2e25819897b010adea
Damien GAMEIRO <dgameiro@e1r12p1.42.fr>
2018-09-19 16:19:52 +0200

    leave properly

--- asm/Colleen.s
+++ asm/Colleen.s
@@ -1,6 +1,6 @@
 section .data
 txt:
-db "section .data%ctxt:%cdb %c%s%c%cglobal _main%cextern _printf%csection .text%c_main:%c;commentaire fonction main%cmov rbp, rsp%cmov rcx, 0%cnew_line:%cpush 10%ccmp rcx, 26%cje print%cinc rcx%cjmp new_line%cprint:%clea rdi, [rel txt]%cmov rsi, 10%cmov rdx, 10%cmov rcx, 34%clea r8, [rel txt]%cmov r9, 34%cxor rax, rax%ccall _printf%cmov rsp, rbp%cret%c;commentaire hors fonction%c"
+db "section .data%ctxt:%cdb %c%s%c%cglobal _main%cextern _printf%csection .text%c_main:%c;commentaire fonction main%cpush rbp%cmov rbp, rsp%cmov rcx, 0%cnew_line:%cpush 10%ccmp rcx, 29%cje print%cinc rcx%cjmp new_line%cprint:%clea rdi, [rel txt]%cmov rsi, 10%cmov rdx, 10%cmov rcx, 34%clea r8, [rel txt]%cmov r9, 34%cxor rax, rax%ccall _printf%cmov rsp, rbp%cleave%cret%c;commentaire hors fonction%c"
 global _main
 extern _printf
 section .text
@@ -11,7 +11,7 @@ mov rbp, rsp
 mov rcx, 0
 new_line:
 push 10
-cmp rcx, 26
+cmp rcx, 29
 je print
 inc rcx
 jmp new_line
@@ -23,9 +23,8 @@ mov rcx, 34
 lea r8, [rel txt]
 mov r9, 34
 xor rax, rax
-push r12
 call _printf
 mov rsp, rbp
-pop rbp
+leave
 ret
 ;commentaire hors fonction
