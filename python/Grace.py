c718b8bbb768de798dfc1e6967f5dcc87e344916
Damien GAMEIRO <dgameiro@e1r12p1.42.fr>
2018-09-19 13:09:31 +0200

    rename funct

--- python/Grace.py
+++ python/Grace.py
@@ -4,10 +4,10 @@
 define_1 = 'Grace_kid.py'
 define_2 = 'w+'
 
-def print_it(X):
+def grace(X):
  f = open(define_1, define_2)
- s = "%c%c%c%c commentaire hors fonction%c%c%c%c%cdefine_1 = 'Grace_kid.py'%cdefine_2 = 'w+'%c%cdef print_it(X):%c f = open(define_1, define_2)%c s = %c%s%c%c f.write(s %c (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))%c f.close()%c%cprint_it(1)%c"
+ s = "%c%c%c%c commentaire hors fonction%c%c%c%c%cdefine_1 = 'Grace_kid.py'%cdefine_2 = 'w+'%c%cdef grace(X):%c f = open(define_1, define_2)%c s = %c%s%c%c f.write(s %c (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))%c f.close()%c%cgrace(1)%c"
  f.write(s % (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))
  f.close()
 
-print_it(1)
+grace(1)
