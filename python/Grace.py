f70f1d6a91549d72ce674aaaf737a01ab7bb5550
Damien GAMEIRO <dgameiro@e1r12p1.42.fr>
2018-09-19 11:39:23 +0200

    close the file

--- python/Grace.py
+++ python/Grace.py
@@ -6,7 +6,8 @@ define_2 = 'w'
 
 def print_it(X):
  f = open(define_1, define_2)
- s = "%c%c%c%c commentaire hors fonction%c%c%c%c%cdefine_1 = 'Grace_kid.py'%cdefine_2 = 'w'%c%cdef print_it(X):%c f = open(define_1, define_2)%c s = %c%s%c%c f.write(s %c (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10))%c%cprint_it(1)%c"
- f.write(s % (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10))
+ s = "%c%c%c%c commentaire hors fonction%c%c%c%c%cdefine_1 = 'Grace_kid.py'%cdefine_2 = 'w'%c%cdef print_it(X):%c f = open(define_1, define_2)%c s = %c%s%c%c f.write(s %c (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))%c f.close()%c%cprint_it(1)%c"
+ f.write(s % (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))
+ f.close()
 
 print_it(1)
