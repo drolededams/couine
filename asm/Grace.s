494bbf71826e4fd1a253529f0fa9e15c991c48fb
Damien GAMEIRO <dgameiro@e1r12p1.42.fr>
2018-09-19 16:08:02 +0200

    Grace final

--- asm/Grace.s
+++ asm/Grace.s
@@ -1,5 +1,5 @@
 section .data
-txt: db "section .data%ctxt: db %c%s%c, 0%cfile: db %cGrace_kid.s%c, 0%cflag: db %cw+%c, 0%cglobal _main%cextern _fprintf%cextern _fopen%cextern _fclose%csection .text%c%%define NL 10%c%%define QUOTE 34%c%%define NUM_OF_NL 36%c%%macro grace 1%c_main:%clea rdi, [rel file]%clea rsi, [rel flag]%ccall _fopen%cpush rbp%cmov rbp, rsp%cmov rcx, 0%cnew_line:%cpush NL%ccmp rcx, NUM_OF_NL%cje print%cinc rcx%cjmp new_line%cprint:%cpush QUOTE%cpush QUOTE%cpush NL%cpush QUOTE%cpush QUOTE%cmov rdi, rax%clea rsi, [rel txt]%c%mov rdx, NL%cmov rcx, QUOTE%clea r8 [rel txt]%cmov r9, QUOTE%cxor rax, rax%cpush r12%ccall _fprintf%cmov rsp, rbp%cleave%cret%c%%endmacro%c;fin macro%cstart:%cgrace x%c", 0
+txt: db "section .data%ctxt: db %c%s%c, 0%cfile: db %cGrace_kid.s%c, 0%cflag: db %cw+%c, 0%cglobal _main%cextern _fprintf%cextern _fopen%cextern _fclose%csection .text%c%%define NL 10%c%%define QUOTE 34%c%%define NUM_OF_NL 45%c%%macro grace 1%c_main:%cpush rbp%cmov rbp, rsp%clea rdi, [rel file]%clea rsi, [rel flag]%ccall _fopen%cmov rcx, 0%cnew_line:%cpush NL%ccmp rcx, NUM_OF_NL%cje print%cinc rcx%cjmp new_line%cprint:%cpush QUOTE%cpush QUOTE%cpush NL%cpush QUOTE%cpush QUOTE%cpush NL%cmov rdi, rax%clea rsi, [rel txt]%c%mov rdx, NL%cmov rcx, QUOTE%clea r8, [rel txt]%cmov r9, QUOTE%cxor rax, rax%ccall _fprintf%cmov rsp, rbp%cleave%cret%c%%endmacro%c;fin macro%cstart:%cgrace x%c", 0
 file: db "Grace_kid.s", 0
 flag: db "w+", 0
 global _main
@@ -9,7 +9,7 @@ extern _fclose
 section .text
 %define NL 10
 %define QUOTE 34
-%define NUM_OF_NL 43
+%define NUM_OF_NL 45
 %macro grace 1
 _main:
 push rbp
@@ -30,6 +30,7 @@ push QUOTE
 push NL
 push QUOTE
 push QUOTE
+push NL
 mov rdi, rax
 lea rsi, [rel txt]
 mov rdx, NL
@@ -37,7 +38,6 @@ mov rcx, QUOTE
 lea r8, [rel txt]
 mov r9, QUOTE
 xor rax, rax
-push r12
 call _fprintf
 mov rsp, rbp
 leave
