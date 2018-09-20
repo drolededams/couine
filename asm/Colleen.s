86823d59e384c2711715fa8888bb8fd7f53679ff
Damien GAMEIRO <dgameiro@e1r11p1.42.fr>
2018-09-20 18:04:39 +0200

    fix 3 define py

--- asm/Colleen.s
+++ asm/Colleen.s
@@ -1,6 +1,6 @@
 section .data
 txt:
-db "section .data%ctxt:%cdb %c%s%c%cglobal _main%cglobal _display%cextern _printf%csection .text%c_display:%cpush rbp%cmov rbp, rsp%cmov rcx, 0%cnew_line:%cpush 10%ccmp rcx, 35%cje print%cinc rcx%cjmp new_line%cprint:%clea rdi, [rel txt]%cmov rsi, 10%cmov rdx, 10%cmov rcx, 34%clea r8, [rel txt]%cmov r9, 34%cxor rax, rax%ccall _printf%cleave%cret%c_main:%c;commentaire fonction main%cpush rbp%cmov rbp, rsp%ccall _display%cleave%cret%c;commentaire hors fonction%c"
+db "section .data%ctxt:%cdb %c%s%c, 0%cglobal _main%cglobal _display%cextern _printf%csection .text%c_display:%cpush rbp%cmov rbp, rsp%cmov rcx, 0%cnew_line:%cpush 10%ccmp rcx, 35%cje print%cinc rcx%cjmp new_line%cprint:%clea rdi, [rel txt]%cmov rsi, 10%cmov rdx, 10%cmov rcx, 34%clea r8, [rel txt]%cmov r9, 34%cxor rax, rax%ccall _printf%cleave%cret%c_main:%c;commentaire fonction main%cpush rbp%cmov rbp, rsp%ccall _display%cleave%cret%c;commentaire hors fonction%c", 0
 global _main
 global _display
 extern _printf
--- python/Grace.py
+++ python/Grace.py
@@ -1,13 +1,14 @@
 """
  commentaire hors fonction
 """
-define_1 = 'Grace_kid.py'
-define_2 = 'w+'
+define_1 = 'Grace_kid'
+define_2 = '.py'
+define_3 = 'w+'
 
 def grace(X):
- f = open(define_1, define_2)
- s = "%c%c%c%c commentaire hors fonction%c%c%c%c%cdefine_1 = 'Grace_kid.py'%cdefine_2 = 'w+'%c%cdef grace(X):%c f = open(define_1, define_2)%c s = %c%s%c%c f.write(s %c (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))%c f.close()%c%cgrace(1)%c"
- f.write(s % (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))
+ f = open(define_1 + define_2, define_3)
+ s = "%c%c%c%c commentaire hors fonction%c%c%c%c%cdefine_1 = 'Grace_kid'%cdefine_2 = '.py'%cdefine_3 = 'w+'%c%cdef grace(X):%c f = open(define_1 + define_2, define_3)%c s = %c%s%c%c f.write(s %c (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))%c f.close()%c%cgrace(1)%c"
+ f.write(s % (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))
  f.close()
 
 grace(1)
