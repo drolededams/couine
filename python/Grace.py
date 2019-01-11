8c1f1aa62f3b5eb94a375ae034c66028f5d61617
Damien GAMEIRO <dgameiro@e1r12p23.42.fr>
2019-01-11 11:14:37 +0100

    PY : SUlly and Grace compatible py 3

--- python/Grace.py
+++ python/Grace.py
@@ -9,9 +9,9 @@ def grace(X):
  try:
   f = open(define_1 + define_2, define_3)
  except:
-  print "Error: Can't open Grace_kid.py"
+  print ("Error: Can't open Grace_kid.py")
   return
- s = "%c%c%c%c commentaire hors fonction%c%c%c%c%cdefine_1 = 'Grace_kid'%cdefine_2 = '.py'%cdefine_3 = 'w+'%c%cdef grace(X):%c try:%c  f = open(define_1 + define_2, define_3)%c except:%c  print %cError: Can't open Grace_kid.py%c%c  return%c s = %c%s%c%c f.write(s %c (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))%c f.close()%c%cgrace(1)%c"
+ s = "%c%c%c%c commentaire hors fonction%c%c%c%c%cdefine_1 = 'Grace_kid'%cdefine_2 = '.py'%cdefine_3 = 'w+'%c%cdef grace(X):%c try:%c  f = open(define_1 + define_2, define_3)%c except:%c  print (%cError: Can't open Grace_kid.py%c)%c  return%c s = %c%s%c%c f.write(s %c (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))%c f.close()%c%cgrace(1)%c"
  f.write(s % (34, 34, 34, 10, 10, 34, 34, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10))
  f.close()
 
--- python/Sully.py
+++ python/Sully.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
   try:
    f = open('Sully_' + str(i) + '.py', 'w+')
   except:
-   print "Error: Can't open file."
+   print ("Error: Can't open file.")
    sys.exit(0)
-  s = "import os%cimport sys%c%cif __name__ == %c__main__%c:%c i = %d%c if __file__.find('_') != -1:%c  i = i - 1%c if i >= 0:%c  try:%c   f = open('Sully_' + str(i) + '.py', 'w+')%c  except:%c   print %cError: Can't open file.%c%c   sys.exit(0)%c  s = %c%s%c%c  f.write(s %c (10, 10, 10, 34, 34, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10, 10))%c  f.close()%c  commamd = 'python Sully_' + str(i) + '.py'%c  if i > 0:%c   os.system(commamd)%c"
+  s = "import os%cimport sys%c%cif __name__ == %c__main__%c:%c i = %d%c if __file__.find('_') != -1:%c  i = i - 1%c if i >= 0:%c  try:%c   f = open('Sully_' + str(i) + '.py', 'w+')%c  except:%c   print (%cError: Can't open file.%c)%c   sys.exit(0)%c  s = %c%s%c%c  f.write(s %c (10, 10, 10, 34, 34, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10, 10))%c  f.close()%c  commamd = 'python Sully_' + str(i) + '.py'%c  if i > 0:%c   os.system(commamd)%c"
   f.write(s % (10, 10, 10, 34, 34, 10, i, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 34, s, 34, 10, 37, 10, 10, 10, 10, 10))
   f.close()
   commamd = 'python Sully_' + str(i) + '.py'
